0523548bad71e82d162b5886e7341363b611ddef
unknown <brnmcdnld720@gmail.com>
2023-09-21 21:34:15 -0600

    finished books.js functions

--- public/src/books.js
+++ public/src/books.js
@@ -1,11 +1,29 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find((book) => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const booksOutArray = books.filter((book) => book.borrows[0].returned === false);
+  const booksReturnedArray = books.filter((book) => book.borrows[0].returned === true);
+  return [booksOutArray, booksReturnedArray];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  const borrowersAccounts = book.borrows.reduce((result, borrower) => {
+    const borrowerAccount = accounts.find((account) => account.id === borrower.id);
+    borrowerAccount.returned = borrower.returned;
 
+    result.push(borrowerAccount)
+    return result; 
+  }, []);
+  return borrowersAccounts.slice(0, 10);
+}
+
+// console.log(getBorrowersForBook(books[11], accounts)); // books[11] id = "5f44713245a57860eea8cff9" 
 module.exports = {
   findAuthorById,
   findBookById,
